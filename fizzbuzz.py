c9c28f1067fb8df92cb2baef4369d56d638adc6c
JoonYoungJang <wkdwnsdud35@gmail.com>
2023-02-03 14:22:29 +0900

    feat: Create fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,8 @@
+user_num = int(input('Hello, there! Enter some positive integer: '))
+
+# Iteration
+for i in range(1, user_num + 1):
+    if i % 3 == 0:
+        print('fizz')
+    else:
+        print(f'{i}')
